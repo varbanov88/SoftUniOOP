a5ccc00fcc4cb4dec8198295cbf1d22acfa888d7
varbanov88 <varbanov_88@abv.bg>
2017-06-28 20:39:23 +0300

    DefiningClasses lab solved

---
+++ DefiningClasses/PersonClass/Person.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Person
+{
+    private string name;
+    private int age;
+    private List<BankAccount> accounts;
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.accounts = new List<BankAccount>();
+    }
+
+    public Person(string name, int age, List<BankAccount> accounts)
+        :this(name, age)
+    {
+        this.accounts = accounts;
+    }
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            this.name = value;
+        }
+    }
+
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+
+        set
+        {
+            this.age = value;
+        }
+    }
+
+    public double GetBalance()
+    {
+        return this.accounts.Select(a => a.Balance).Sum();
+    }
+}
+
