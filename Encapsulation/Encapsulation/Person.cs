0ea1303ab4712a7bbf5f021ed1c0105dba59227a
varbanov88 <varbanov_88@abv.bg>
2017-06-30 15:03:10 +0300

    Encapsulation lab

---
+++ Encapsulation/Encapsulation/Person.cs
@@ -0,0 +1,23 @@
+﻿public class Person
+{
+    public Person(string firstName, string lastName, int age)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    private string firstName;
+    private string lastName;
+    private int age;
+
+    public string FirstName => this.firstName;
+
+    public int Age => this.age;
+
+    public override string ToString()
+    {
+        return $"{this.firstName} {this.lastName} is a {this.age} years old";
+    }
+}
+
---
+++ Encapsulation/Salary/Person.cs
@@ -0,0 +1,40 @@
+﻿public class Person
+{
+    public Person(string firstName, string lastName, int age, double salary)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    private string firstName;
+    private string lastName;
+    private int age;
+    private double salary;
+
+    public string FirstName => this.firstName;
+
+    public int Age => this.age;
+
+    public double Salary => this.salary;
+
+    public void IncreaseSalary(double bonus)
+    {
+        if (this.Age < 30)
+        {
+            this.salary += this.salary * bonus / 200;
+        }
+
+        else
+        {
+            this.salary += this.salary * bonus / 100;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{this.firstName} {this.lastName} get {this.salary:f2} leva";
+    }
+}
+
