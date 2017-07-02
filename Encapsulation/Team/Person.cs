f8f5da5bccaef75d8ef91bbb35ab8a4fd93dbe42
varbanov88 <varbanov_88@abv.bg>
2017-07-02 12:45:57 +0300

    encapsulation lab solved

--- Encapsulation/Team/Person.cs
+++ Encapsulation/Team/Person.cs
@@ -1,18 +1,86 @@
-﻿public class Person
+﻿using System;
+
+public class Person
 {
-    private string firtsName;
+    private string firstName;
     private string lastName;
     private int age;
     private double salary;
 
     public Person(string firtsName, string lastName, int age, double salary)
     {
-        this.firtsName = firtsName;
-        this.lastName = lastName;
-        this.age = age;
-        this.salary = salary;
+        this.FirstName = firtsName;
+        this.LastName = lastName;
+        this.Age = age;
+        this.Salary = salary;
     }
 
-    public int Age { get { return this.age; } }
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        private set
+        {
+            if (value < 1)
+            {
+                throw new ArgumentException("Age cannot be zero or negative integer");
+            }
+
+            this.age = value;
+        }
+    }
+
+    public double Salary
+    {
+        get
+        {
+            return this.salary;
+        }
+        private set
+        {
+            if (value < 460)
+            {
+                throw new ArgumentException("Salary cannot be less than 460 leva");
+            }
+
+            this.salary = value;
+        }
+    }
+
+    public string FirstName
+    {
+        get
+        {
+            return this.firstName;
+        }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot be less than 3 symbols");
+            }
+
+            this.firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get
+        {
+            return this.lastName;
+        }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot be less than 3 symbols");
+            }
+
+            this.lastName = value;
+        }
+    }
 }
 
