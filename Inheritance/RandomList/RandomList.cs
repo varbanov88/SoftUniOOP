9e0cd056e2d757d634707da3f19bb05b89a08926
varbanov88 <varbanov_88@abv.bg>
2017-07-04 21:16:18 +0300

    Inheritance lab 4/5 solved

---
+++ Inheritance/RandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomList : ArrayList
+{
+    public RandomList()
+    {
+        this.rnd = new Random();
+        this.data = new List<string>();
+    }
+
+    private Random rnd;
+    private List<string> data;
+
+    public object RandomString()
+    {
+        int element = rnd.Next(0, this.data.Count - 1);
+        string str = data[element];
+        data.Remove(str);
+        return str;
+    }
+}
+
---
+++ Inheritance/SingleInheritance/StartUp.cs
@@ -0,0 +1,11 @@
+﻿public class StartUp
+{
+    public static void Main()
+    {
+        var dog = new Dog();
+
+        dog.Eat();
+        dog.Bark();
+    }
+}
+
