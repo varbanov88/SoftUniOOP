919432d6f29ddc26300d7a5b99e2492af6d0e59a
varbanov88 <varbanov_88@abv.bg>
2017-07-20 19:25:12 +0300

    interfaces lab solved

---
+++ InterfacesAndAbstraction/Cars/Seat.cs
@@ -0,0 +1,28 @@
+﻿public class Seat : ICar
+{
+    public Seat(string model, string color)
+    {
+        this.Model = model;
+        this.Color = color;
+    }
+
+    public string Model { get; private set; }
+
+    public string Color { get; private set; }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+
--- InterfacesAndAbstraction/Shapes/StartUp.cs
+++ InterfacesAndAbstraction/Shapes/StartUp.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Shapes
 {
@@ -10,6 +6,15 @@ namespace Shapes
     {
         public static void Main()
         {
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height);
+
+            circle.Draw();
+            rect.Draw();
         }
     }
 }
