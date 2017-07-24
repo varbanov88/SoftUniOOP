b92ea7dc2109b6f0e7d7722d4969c6fa3cb93588
varbanov88 <varbanov_88@abv.bg>
2017-07-24 20:59:05 +0300

    Generics lab solved

---
+++ Generics/Box/Box.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Box<T>
+{
+    private List<T> data;
+
+    public Box()
+    {
+        this.data = new List<T>();
+    }
+
+    public int Count => this.data.Count;
+
+    public void Add(T element)
+    {
+        this.data.Insert(0, element);
+    }
+
+    public T Remove()
+    {
+        var element = this.data.First();
+        this.data.RemoveAt(0);
+        return element;
+    }
+}
+
