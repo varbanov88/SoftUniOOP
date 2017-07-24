9939e00b910f96abf15dabdb8fc4071147e05c7b
varbanov88 <varbanov_88@abv.bg>
2017-07-25 00:03:57 +0300

    Generics exercises 9/12 solved

---
+++ Generics/CustomList/Sorter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomList
+{
+    public class Sorter
+    {
+        public static CustomList<T> Sort<T>(CustomList<T> customList)
+            where T : IComparable<T>
+        {
+            var temp = customList.Elements.OrderBy(x => x);
+            return new CustomList<T>(temp);
+        }
+    }
+}
---
+++ Generics/GenericBoxProblems/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericBoxProblems
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfLines = int.Parse(Console.ReadLine());
+            IList<Box<double>> listOfBoxes = new List<Box<double>>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                Box<double> box = new Box<double>(double.Parse(Console.ReadLine()));
+                listOfBoxes.Add(box);
+            }
+
+            var element = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetGreaterElementCount(listOfBoxes, element));
+        }
+
+        public static int GetGreaterElementCount<T>(IList<Box<T>> list, T element)
+            where T : IComparable        
+            => list.Count(b => b.Item.CompareTo(element) > 0);
+    }
+}
