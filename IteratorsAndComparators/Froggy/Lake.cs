3eaff873ab0727d42379f6b1adde5391014908fb
varbanov88 <varbanov_88@abv.bg>
2017-07-30 18:12:59 +0300

    Froggy task

---
+++ IteratorsAndComparators/Froggy/Lake.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private readonly List<int> stones;
+
+        public Lake(IEnumerable<int> stones)
+        {
+            this.stones = new List<int>(stones);
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < this.stones.Count; i += 2)
+            {
+                yield return this.stones[i];
+            }
+
+            var lastIndex = this.stones.Count - 1;
+            if (lastIndex % 2 == 0)
+            {
+                lastIndex--;
+            }
+
+            for (int i = lastIndex; i > 0; i -= 2)
+            {
+                yield return this.stones[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IteratorsAndComparators/Froggy/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Froggy
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputStones = Console.ReadLine()
+                                     .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                                     .Select(int.Parse)
+                                     .ToArray();
+
+            var lake = new Lake(inputStones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
