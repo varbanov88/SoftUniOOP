d5688d1435593eda594dd143d8e9cfe5868c9d0d
varbanov88 <varbanov_88@abv.bg>
2017-07-31 20:52:37 +0300

    EnumerationsAndAttributes lab solved

---
+++ EnumerationsAndAttributes/CoffeeMachinee/ICoffeeMachine.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+public interface ICoffeeMachine
+{
+    void BuyCoffee(string size, string type);
+    void InsertCoin(string coin);
+    IEnumerable<CoffeeType> CoffeesSold { get; }
+}
---
+++ EnumerationsAndAttributes/WeekDay/WeeklyCalendar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+public class WeeklyCalendar
+{
+    private IList<WeeklyEntry> entries;
+
+    public WeeklyCalendar()
+    {
+        this.entries = new List<WeeklyEntry>();
+    }
+
+    public IEnumerable<WeeklyEntry> WeeklySchedule => this.entries;
+
+    public void AddEntry(string weekday, string notes)
+    {
+        this.entries.Add(new WeeklyEntry(weekday, notes));
+    }
+}
+
